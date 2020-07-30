0cfc221b850f16492870ba603fe3ae1ce87b5ae7
gitsherin <68219136+gitsherin@users.noreply.github.com>
2020-07-30 23:18:58 +0530

    Update gke.sh

--- gke.sh
+++ gke.sh
@@ -7,5 +7,3 @@ sudo apt-get install apt-transport-https ca-certificates gnupg -y
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 
 sudo apt-get update && sudo apt-get install google-cloud-sdk -y
-
-gcloud init
